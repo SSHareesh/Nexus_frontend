a950d6653dbc656118bb6b663b88407ffd84bb7e
unknown <jai06032005@gmail.com>
2025-02-17 15:32:01 +0530

    hello'

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
-// import NavBar from './components/layout/Navbar';
 import HardwareAssets from './pages/HardwareAssets/HardwareAssets';
-
 
 function App() {
   return (
-    <Router>
+    <Router
+      future={{
+        v7_startTransition: true,  // Opt-in to React v7 transition behavior
+      }}
+    >
       <Routes>
         <Route path="/" element={<Layout><Dashboard /></Layout>} />
-        <Route path='/HardwareAssets' element={<Layout><HardwareAssets /></Layout>} />
+        <Route path="/HardwareAssets" element={<Layout><HardwareAssets /></Layout>} />
       </Routes>
     </Router>
   );
